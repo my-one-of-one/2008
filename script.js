f1c61a2f307c75257f3b5c1970d455049db7e07b
ro <robertacaputo49@gmail.com>
2026-03-20 21:04:38 +0100

    Descrizione delle modifiche

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const pixelLetter = document.getElementById('pixel-letter');
 const introScreen = document.getElementById('intro-screen');
 const letterScreen = document.getElementById('letter-screen');
+const nextButton = document.getElementById('next-button');
+const letterScreen2 = document.getElementById('letter-screen-2');
 
+// Mostra la prima lettera
 pixelLetter.addEventListener('click', () => {
-  // Fade out intro
   introScreen.classList.add('hidden');
-  
-  // Attendi mezzo secondo per fare fade in lettera
   setTimeout(() => {
     letterScreen.classList.remove('hidden');
   }, 500);
+});
+
+// Passaggio alla seconda schermata (P.S.)
+nextButton.addEventListener('click', () => {
+  letterScreen.classList.add('hidden');
+  letterScreen2.classList.remove('hidden');
 });
